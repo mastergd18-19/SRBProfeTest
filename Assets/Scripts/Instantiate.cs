ebd2b8ad667c41477b6d99e69f3f21c9a1b6c0ed
NeoSanti <santiago.rodriguez.bedate@gmail.com>
2019-05-07 21:43:36 +0200

    .

---
+++ Assets/Scripts/Instantiate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+
+    public Transform t;
+    public GameObject go;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootSphere();
+    }
+
+    void ShootSphere()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(go, t.position, t.rotation);
+        }
+    }
+
+}
