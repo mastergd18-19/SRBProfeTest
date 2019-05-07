2d479424fc165ab96fd71a66ec2e2d5394dc8ac0
NeoSanti <santiago.rodriguez.bedate@gmail.com>
2019-05-07 17:11:30 +0200

    - Movements

---
+++ Assets/Scripts/PlayerMovement_Physics.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_Physics : MonoBehaviour
+{
+    public float acceleration;
+    public float decceleration;
+    public bool moving;
+
+    public float t;
+    public float rR;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        InvokeRepeating("CheckSpeed", t, rR);
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        MovementDecceleration();
+    }
+
+    void Movement()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal"); // GetAxis suma 0.05f, GetAxisRaw suma 1.
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        //if(rb.velocity.x != 0 | rb.velocity.y != 0 | rb.velocity.z != 0) // Comprueba las velocidades, rara vez se quedan a 0.
+        //if (moveHorizontal < 0.0f | moveVertical < 0.0f) // Comprueba los valores, al cabo de un segundo de dejar de pulsar se quedan en 0.
+        if (Input.GetAxis("Horizontal") != 0.0f | Input.GetAxis("Vertical") != 0.0f) // Comprueba si hay input, perfecto.
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * acceleration);
+    }
+
+    void MovementDecceleration()
+    {
+        if(moving == false)
+        {
+            rb.angularDrag = decceleration;
+        } else
+        {
+            rb.angularDrag = 0.05f;
+        }
+    }
+
+    void CheckSpeed()
+    {
+        Debug.Log("Velocidad en x: " + rb.velocity.x);
+        Debug.Log("Velocidad en y: " + rb.velocity.y);
+        Debug.Log("Velocidad en z: " + rb.velocity.z);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement_Traslation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_Traslation : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        }
+
+    }
+}
